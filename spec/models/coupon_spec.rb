a35965018c30639cc4a5fdcc2196f7ddd75f7e88
Linda Le <yueling1222@gmail.com>
2020-01-14 13:42:09 -0700

    Add model spec for coupon

---
+++ spec/models/coupon_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe Coupon, type: :model do
+    describe "validations" do
+        it { should validate_presence_of :name }
+        it { should validate_presence_of :code }
+        it { should validate_presence_of :percent_off }
+        it { should validate_uniqueness_of :name}
+        it { should validate_uniqueness_of :code}
+    end
+
+    describe "relationships" do
+        it {should belong_to :merchant}
+        it {should have_many(:orders).through(:coupon_orders)}
+    end
+
+end
