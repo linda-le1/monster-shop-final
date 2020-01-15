01b6b75e58396a1184d6ef3880d3dd6b279a1d9c
Linda Le <yueling1222@gmail.com>
2020-01-14 17:18:01 -0700

    Switch out double quotes for single per HoundCI

--- spec/features/merchant/coupons/new_spec.rb
+++ spec/features/merchant/coupons/new_spec.rb
@@ -6,8 +6,8 @@ RSpec.describe 'As a merchant' do
             @merchant = create(:random_merchant)
             @merchant_2 = create(:random_merchant)
             @merchant_user = create(:random_user, role: 3, merchant_id: @merchant.id)
-            @coupon_1 = Coupon.create(name: "10 Percent Off Total Purchase", code: "10OFF", percent_off: 0.10)
-            @coupon_2 = Coupon.create(name: "20 Percent Off Total Purchase", code: "20OFF", percent_off: 0.20)
+            @coupon_1 = Coupon.create(name: '10 Percent Off Total Purchase', code: '10OFF', percent_off: 0.10)
+            @coupon_2 = Coupon.create(name: '20 Percent Off Total Purchase', code: '20OFF', percent_off: 0.20)
             @merchant.coupons << @coupon_1
             @merchant_2.coupons << @coupon_2
 
